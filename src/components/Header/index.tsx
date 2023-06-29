d2b21b7d51a3390997508d821af59c40a3038f20
mgckaled <marcelgckaled@gmail.com>
2023-06-29 11:50:02 -0300

    fix: remove cart icon from success page

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,26 +1,29 @@
 import Image from "next/image"
 import Link from "next/link"
+import { useRouter } from "next/router"
 
 import logoImg from "../../assets/logo.svg"
-
 import { Cart } from "../Cart"
+
 import { HeaderContainer } from "./styles"
 
 export function Header() {
+  const { pathname } = useRouter()
+
+  const showCartButton = pathname !== "/success"
+
   return (
     <HeaderContainer>
-      <HeaderContainer>
-        <Link href={"/"}>
-          <Image
-            src={logoImg}
-            alt='logo app image: Ignite Shop'
-            width={130}
-            height={52}
-            priority
-          />
-        </Link>
-        <Cart />
-      </HeaderContainer>
+      <Link href={"/"}>
+        <Image
+          src={logoImg}
+          alt='logo app image: Ignite Shop'
+          width={130}
+          height={52}
+          priority
+        />
+      </Link>
+      {showCartButton && <Cart />}
     </HeaderContainer>
   )
 }
